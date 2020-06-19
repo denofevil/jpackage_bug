696d4670417691208a0dfc6ff0ca6f12290f88a2
Dennis Ushakov <dennis.ushakov@gmail.com>
2020-06-19 13:33:43 +0300

    fix prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,9 @@
+mkdir target
 cd src
 javac Example.java
 jar cmf META-INF/MANIFEST.MF ../target/example.jar Example.class
 cd ..
 jpackage --input target/ --name example  --main-jar example.jar --type dmg
-open example-1.0.dmg
+hdiutil attach example-1.0.dmg
 cp -r /Volumes/example/example.app .
 ln -s example.app/Contents/MacOS/example example
